bc2575d7f8b70339db623904b75b63359efc8282
ding11m <n4v2b@ugrad.cs.ubc.ca>
2020-06-25 04:47:28 -0700

    add schemas

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
+const restify = require("restify");
+
+
 const  mongoose = require("mongoose");
 const  User = require("./models/user");
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost/shop_app");
-const db = mongoose.connection;
-db.on("open", async ()=> {
-    console.log("connected!");
-    const user = new User({
-        userName: "test",
-        password: "123",
-        nickName: "Alex",
-        avatar: "/uploads/avatar.jpg"
+
+const app = restify.createServer();
+
+app.get("/", (req, res, next) => {
+    res.send(200, {
+        code: "success",
+        message: "connected!"
     });
-        const userSave = await user.save();
-        console.log(userSave);
+    next();
 });
-db.on("error",(error) => {
-    console.log("error");
-});
+
+app.get('/users',async (req, res, next) => {
+    const users = await User.find({});
+    res.send(users);
+    next();
+})
+
+app.listen(3005, () => {
+    mongoose.set('useNewUrlParser', true);
+    mongoose.set('useFindAndModify', false);
+    mongoose.set('useCreateIndex', true);
+    mongoose.set('useUnifiedTopology', true);
+    mongoose.connect("mongodb://localhost/shop_app");
+    const db = mongoose.connection;
+    db.on("open", async ()=> {
+        const example = new User({userName: "test", password: "123"});
+        const save = await example.save();
+        console.log("connected!");
+    });
+    db.on("error",(error) => {
+        console.log("error");
+    });
+
+})
+
+
